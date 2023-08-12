52baf510c8541c01aee6ec184ab21bb1a86c59d4
Barry Fang <barryscompany@gmail.com>
2023-08-12 17:40:02 +0800

    calc avg len of reviews

--- read.py
+++ read.py
@@ -6,8 +6,9 @@ with open ('reviews.txt', 'r') as f:
         count += 1 # 快寫法 完全等於 count = count + 1
         if count % 1000 == 0: # %是求餘數的意思 count跟1000的餘數 = 0
             print(len(data))
-print(len(data))
+print('檔案讀取完了,總共有', len(data), '筆資料') 
 
-print(data[0])
-print('-------------')
-print(data[1])
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print('留言的平均長度', sum_len/len(data))
